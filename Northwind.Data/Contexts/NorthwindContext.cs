6789f749ebb5c9bf7f2b37eb969b76c4fae22a53
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-06-28 14:12:49 -0400

    Data: Setup Sqlite3 and Add Northwind DB context

---
+++ Northwind.Data/Contexts/NorthwindContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Data.Entities;
+
+namespace Northwind.Data.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        public NorthwindContext(DbContextOptions<NorthwindContext> options) : base(options)
+        {
+        }
+
+        // TODO: remove this method and setup DB connection in API project
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
+                optionsBuilder.UseSqlite($"Filename={path}")
+                              .EnableSensitiveDataLogging();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                        .HasIndex(e => new { e.FirstName, e.LastName })
+                        .HasName("FullName");
+
+            modelBuilder.Entity<OrderDetail>()
+                        .ToTable("Order Details")
+                        .HasKey(od => new { od.OrderId, od.ProductId });
+
+            modelBuilder.Entity<Order>()
+                        .Property(o => o.ShipperId)
+                        .HasColumnName("ShipVia");
+        }
+    }
+}
--- Northwind.Data/Program.cs
+++ Northwind.Data/Program.cs
@@ -2,6 +2,10 @@
 
 namespace Northwind.Data
 {
+    /*
+     * TODO: More complex Northwind DB schema: https://github.com/jpwhite3/northwind-SQLite3
+     */
+
     class Program
     {
         static void Main(string[] args)
