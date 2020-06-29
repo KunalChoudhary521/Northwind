d7d238e0a3206e07b1c28c037f0a621b8dde3b54
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-06-29 13:02:11 -0400

    API: Initial commit

---
+++ Northwind.API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Data.Entities;
+
+namespace Northwind.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<Product[]>> GetProducts()
+        {
+            try
+            {
+                return new Product[]
+                {
+                    new Product { ProductName = "Beverages" }
+                };
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
