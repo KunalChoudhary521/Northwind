6b5492d23e22323a96b684bf976be9988d6b8960
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-07-13 18:48:02 -0400

    Data: Add Locations migration

---
+++ Northwind.Data/Migrations/20200713223542_Locations.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Northwind.Data.Migrations
+{
+    public partial class Locations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Fax",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Suppliers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LocationId",
+                table: "Suppliers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    LocationId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Address = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Region = table.Column<string>(nullable: true),
+                    PostalCode = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Extension = table.Column<string>(nullable: true),
+                    Fax = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.LocationId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suppliers_LocationId",
+                table: "Suppliers",
+                column: "LocationId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locations_Address",
+                table: "Locations",
+                column: "Address",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Suppliers_Locations_LocationId",
+                table: "Suppliers",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Suppliers_Locations_LocationId",
+                table: "Suppliers");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Suppliers_LocationId",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Suppliers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Suppliers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Suppliers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Suppliers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Fax",
+                table: "Suppliers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Suppliers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Suppliers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Suppliers",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
