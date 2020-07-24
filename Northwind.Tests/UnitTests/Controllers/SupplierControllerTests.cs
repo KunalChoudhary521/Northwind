6f2a1c149275e76f32dea80a38f4d5476a504c2b
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-07-23 20:11:23 -0400

    Tests: Add supplier product service and controller tests

--- Northwind.Tests/UnitTests/Controllers/SupplierControllerTests.cs
+++ Northwind.Tests/UnitTests/Controllers/SupplierControllerTests.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
+using Hellang.Middleware.ProblemDetails;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -15,17 +17,19 @@ namespace Northwind.Tests.UnitTests.Controllers
 {
     public class SupplierControllerTests
     {
-        private readonly Mock<IService<Supplier>> _supplierService;
+        private readonly Mock<ISupplierService> _supplierService;
         private readonly SuppliersController _suppliersController;
 
         public SupplierControllerTests()
         {
             var mapperConfig = new MapperConfiguration(config =>
             {
-                config.AddProfiles(new Profile[] { new SupplierProfile(), new LocationProfile() });
+                config.AddProfiles(new Profile[] { new SupplierProfile(),
+                                                   new LocationProfile(),
+                                                   new ProductProfile() });
             });
             var mapper = mapperConfig.CreateMapper();
-            _supplierService = new Mock<IService<Supplier>>();
+            _supplierService = new Mock<ISupplierService>();
 
             _suppliersController = new SuppliersController(_supplierService.Object, mapper);
         }
@@ -54,7 +58,7 @@ namespace Northwind.Tests.UnitTests.Controllers
         }
 
         [Fact]
-        public async Task FailedToAddSupplier_AddSupplier_ReturnBadRequest()
+        public async Task FailedToSaveNewSupplier_AddSupplier_ReturnBadRequest()
         {
             var supplierModel = new SupplierModel();
 
@@ -70,8 +74,7 @@ namespace Northwind.Tests.UnitTests.Controllers
         {
             var supplierModel = new SupplierModel();
 
-            _supplierService.Setup(s => s.Add(It.IsAny<Supplier>()))
-                            .Returns(Task.FromResult(new Supplier { SupplierId = 30 }));
+            _supplierService.Setup(s => s.IsSavedToDb()).Returns(Task.FromResult(true));
 
             var response = await _suppliersController.AddSupplier(supplierModel);
 
@@ -80,8 +83,7 @@ namespace Northwind.Tests.UnitTests.Controllers
 
             Assert.IsType<SupplierModel>(createdAtActionResult.Value);
             Assert.Equal(StatusCodes.Status201Created, createdAtActionResult.StatusCode);
-            Assert.Single(createdAtActionResult.RouteValues.Keys);
-            Assert.Equal(30, createdAtActionResult.RouteValues["supplierId"]);
+            Assert.Single(createdAtActionResult.RouteValues.Keys, "supplierId");
         }
 
         [Fact]
@@ -97,7 +99,7 @@ namespace Northwind.Tests.UnitTests.Controllers
         }
 
         [Fact]
-        public async Task FailedToUpdateSupplier_UpdateSupplier_ReturnBadRequest()
+        public async Task FailedToSaveUpdateSupplier_UpdateSupplier_ReturnBadRequest()
         {
             const int supplierId = 40;
             var supplierModel = new SupplierModel();
@@ -121,6 +123,197 @@ namespace Northwind.Tests.UnitTests.Controllers
             var response = await _suppliersController.DeleteSupplier(supplierId);
 
             Assert.IsType<NotFoundResult>(response);
+        }
+
+        [Fact]
+        public async Task IdOfNonexistentSupplier_GetSupplierProducts_ReturnNotFound()
+        {
+            const int supplierId = -1;
+
+            var exception = await Assert.ThrowsAsync<ProblemDetailsException>(() =>
+                _suppliersController.GetSupplierProducts(supplierId));
+
+            Assert.Equal(StatusCodes.Status404NotFound, exception.Details.Status);
+            Assert.Contains("supplier", exception.Details.Title, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public async Task IdOfNonexistentSupplier_GetSupplierProduct_ReturnNotFound()
+        {
+            const int supplierId = -1;
+            const int productId = 10;
+
+            var exception = await Assert.ThrowsAsync<ProblemDetailsException>(() =>
+                _suppliersController.GetSupplierProduct(supplierId, productId));
+
+            Assert.Equal(StatusCodes.Status404NotFound, exception.Details.Status);
+            Assert.Contains("supplier", exception.Details.Title, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public async Task IdOfNonexistentProduct_GetSupplierProduct_ReturnNotFound()
+        {
+            const int supplierId = 10;
+            const int productId = -1;
+
+            _supplierService.Setup(s => s.GetById(supplierId))
+                            .Returns(Task.FromResult(new Supplier()));
+
+            var exception = await Assert.ThrowsAsync<ProblemDetailsException>(() =>
+                _suppliersController.GetSupplierProduct(supplierId, productId));
+
+            Assert.Equal(StatusCodes.Status404NotFound, exception.Details.Status);
+            Assert.Contains("product", exception.Details.Title, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public async Task IdOfNonexistentSupplier_AddSupplierProduct_ReturnNotFound()
+        {
+            const int supplierId = -1;
+            var productModel = new ProductModel();
+
+            var exception = await Assert.ThrowsAsync<ProblemDetailsException>(() =>
+                _suppliersController.AddSupplierProduct(supplierId, productModel));
+
+            Assert.Equal(StatusCodes.Status404NotFound, exception.Details.Status);
+            Assert.Contains("supplier", exception.Details.Title, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public async Task FailedToSaveNewProduct_AddSupplierProduct_ReturnBadRequest()
+        {
+            const int supplierId = 10;
+            var productModel = new ProductModel();
+
+            _supplierService.Setup(s => s.GetById(supplierId))
+                            .Returns(Task.FromResult(new Supplier()));
+
+            var response = await _suppliersController.AddSupplierProduct(supplierId, productModel);
+
+            Assert.IsType<ActionResult<ProductModel>>(response);
+            Assert.IsType<BadRequestResult>(response.Result);
+            _supplierService.Verify(s => s.AddProduct(supplierId, It.IsAny<Product>()));
+        }
+
+        [Fact]
+        public async Task ProductModel_AddSupplierProduct_ResponseContainsLocationInHeader()
+        {
+            const int supplierId = 10;
+            var productModel = new ProductModel();
+
+            _supplierService.Setup(s => s.GetById(supplierId))
+                            .Returns(Task.FromResult(new Supplier()));
+            _supplierService.Setup(s => s.IsSavedToDb()).Returns(Task.FromResult(true));
+
+            var response = await _suppliersController.AddSupplierProduct(supplierId, productModel);
+
+            Assert.IsType<ActionResult<ProductModel>>(response);
+            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(response.Result);
+
+            Assert.IsType<ProductModel>(createdAtActionResult.Value);
+            Assert.Equal(StatusCodes.Status201Created, createdAtActionResult.StatusCode);
+            Assert.Equal(2, createdAtActionResult.RouteValues.Keys.Count);
+            Assert.Contains("supplierId", createdAtActionResult.RouteValues.Keys);
+            Assert.Contains("productId", createdAtActionResult.RouteValues.Keys);
+        }
+
+        [Fact]
+        public async Task IdOfNonexistentSupplier_UpdateSupplierProduct_ReturnNotFound()
+        {
+            const int supplierId = -1;
+            const int productId = 10;
+            var productModel = new ProductModel();
+
+            var exception = await Assert.ThrowsAsync<ProblemDetailsException>(() =>
+                _suppliersController.UpdateSupplierProduct(supplierId, productId, productModel));
+
+            Assert.Equal(StatusCodes.Status404NotFound, exception.Details.Status);
+            Assert.Contains("supplier", exception.Details.Title, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public async Task IdOfNonexistentProduct_UpdateSupplierProduct_ReturnNotFound()
+        {
+            const int supplierId = 10;
+            const int productId = -1;
+            var productModel = new ProductModel();
+
+            _supplierService.Setup(s => s.GetById(supplierId))
+                            .Returns(Task.FromResult(new Supplier()));
+
+            var exception = await Assert.ThrowsAsync<ProblemDetailsException>(() =>
+                _suppliersController.UpdateSupplierProduct(supplierId, productId, productModel));
+
+            Assert.Equal(StatusCodes.Status404NotFound, exception.Details.Status);
+            Assert.Contains("product", exception.Details.Title, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public async Task FailedToSaveUpdatedProduct_UpdateSupplierProduct_ReturnBadRequest()
+        {
+            const int supplierId = 10;
+            const int productId = 2;
+            var productModel = new ProductModel();
+
+            _supplierService.Setup(s => s.GetById(supplierId))
+                            .Returns(Task.FromResult(new Supplier()));
+            _supplierService.Setup(s => s.GetProductById(supplierId, productId))
+                            .Returns(Task.FromResult(new Product()));
+
+            var response = await _suppliersController.UpdateSupplierProduct(supplierId,
+                                                                            productId,
+                                                                            productModel);
+
+            Assert.IsType<ActionResult<ProductModel>>(response);
+            Assert.IsType<BadRequestResult>(response.Result);
+            _supplierService.Verify(s => s.UpdateProduct(supplierId, It.IsAny<Product>()));
+        }
+
+        [Fact]
+        public async Task IdOfNonexistentSupplier_DeleteSupplierProduct_ReturnNotFound()
+        {
+            const int supplierId = -1;
+            const int productId = 10;
+
+            var exception = await Assert.ThrowsAsync<ProblemDetailsException>(() =>
+                _suppliersController.DeleteSupplierProduct(supplierId, productId));
+
+            Assert.Equal(StatusCodes.Status404NotFound, exception.Details.Status);
+            Assert.Contains("supplier", exception.Details.Title, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public async Task IdOfNonexistentProduct_DeleteSupplierProduct_ReturnNotFound()
+        {
+            const int supplierId = 10;
+            const int productId = -1;
+
+            _supplierService.Setup(s => s.GetById(supplierId))
+                            .Returns(Task.FromResult(new Supplier()));
+
+            var exception = await Assert.ThrowsAsync<ProblemDetailsException>(() =>
+                _suppliersController.DeleteSupplierProduct(supplierId, productId));
+
+            Assert.Equal(StatusCodes.Status404NotFound, exception.Details.Status);
+            Assert.Contains("product", exception.Details.Title, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public async Task FailedToSaveDeletedProduct_DeleteSupplierProduct_ReturnBadRequest()
+        {
+            const int supplierId = 10;
+            const int productId = 2;
+
+            _supplierService.Setup(s => s.GetById(supplierId))
+                            .Returns(Task.FromResult(new Supplier()));
+            _supplierService.Setup(s => s.GetProductById(supplierId, productId))
+                            .Returns(Task.FromResult(new Product()));
+
+            var response = await _suppliersController.DeleteSupplierProduct(supplierId, productId);
+
+            Assert.IsType<ActionResult<ProductModel>>(response);
+            Assert.IsType<BadRequestResult>(response.Result);
+            _supplierService.Verify(s => s.DeleteProduct(supplierId, It.IsAny<Product>()));
         }
     }
 }
--- Northwind.Tests/UnitTests/Services/SupplierServiceTests.cs
+++ Northwind.Tests/UnitTests/Services/SupplierServiceTests.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MockQueryable.Moq;
 using Moq;
 using Northwind.API.Repositories;
 using Northwind.API.Services;
@@ -11,27 +18,78 @@ namespace Northwind.Tests.UnitTests.Services
     {
         private readonly Mock<IRepository<Supplier>> _supplierRepository;
         private readonly Mock<IRepository<Location>> _locationRepository;
-        private readonly IService<Supplier> _supplierService;
+        private readonly Mock<IRepository<Product>> _productRepository;
+        private readonly ISupplierService _supplierService;
 
         public SupplierServiceTests()
         {
             _supplierRepository = new Mock<IRepository<Supplier>>();
             _locationRepository = new Mock<IRepository<Location>>();
-            var logger = new Mock<ILogger<IService<Supplier>>>();
+            _productRepository = new Mock<IRepository<Product>>();
+            var logger = new Mock<ILogger<ISupplierService>>();
 
             _supplierService = new SupplierService(_supplierRepository.Object,
-                                                   _locationRepository.Object, logger.Object);
+                                                   _locationRepository.Object,
+                                                   _productRepository.Object,
+                                                   logger.Object);
         }
 
         [Fact]
-        public void Supplier_Delete_DeleteSupplierAndRelatedLocation()
+        public async Task SupplierWithProducts_Delete_DetachProductsDeleteSupplierAndLocation()
         {
             var supplier = new Supplier { Location = new Location() };
 
-            _supplierService.Delete(supplier);
+            var products = new []
+            {
+                new Product { ProductId = 1, SupplierId = 2 },
+                new Product { ProductId = 2, SupplierId = 2 }
+            };
+            var mockProducts = products.AsQueryable().BuildMock();
+
+            _productRepository.Setup(pr => pr.FindByCondition(It.IsAny<Expression<Func<Product, bool>>>()))
+                              .Returns(mockProducts.Object);
+
+            await _supplierService.Delete(supplier);
 
             _supplierRepository.Verify(s => s.Delete(supplier));
             _locationRepository.Verify(l => l.Delete(supplier.Location));
+
+            var detachedProducts = await mockProducts.Object.ToArrayAsync();
+            foreach (var product in detachedProducts)
+                Assert.Null(product.SupplierId);
+        }
+
+        [Fact]
+        public void SupplierIdAndProduct_AddProduct_AttachProductToSupplier()
+        {
+            const int supplierId = 5;
+            var product = new Product();
+
+            _supplierService.AddProduct(supplierId, product);
+
+            Assert.Equal(5, product.SupplierId);
+        }
+
+        [Fact]
+        public void SupplierIdAndProduct_UpdateProduct_AttachProductToSupplier()
+        {
+            const int supplierId = 7;
+            var product = new Product();
+
+            _supplierService.UpdateProduct(supplierId, product);
+
+            Assert.Equal(7, product.SupplierId);
+        }
+
+        [Fact]
+        public void SupplierIdAndProduct_DeleteProduct_DetachProductFromSupplier()
+        {
+            const int supplierId = 7;
+            var product = new Product();
+
+            _supplierService.DeleteProduct(supplierId, product);
+
+            Assert.Null(product.SupplierId);
         }
     }
 }
