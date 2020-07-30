d84f094b08947f94f50fbece2773791dceeedd9d
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-07-29 20:55:08 -0400

    API: Product's CategoryId is optional and SupplierId is required

--- Northwind.API/Models/ProductModel.cs
+++ Northwind.API/Models/ProductModel.cs
@@ -1,10 +1,13 @@
-﻿namespace Northwind.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.API.Models
 {
     public class ProductModel
     {
         public int ProductId { get; set; }
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
         public string ProductName { get; set; }
+        [Required]
         public int SupplierId { get; set; }
         public string QuantityPerUnit { get; set; }
         public decimal UnitPrice { get; set; }
