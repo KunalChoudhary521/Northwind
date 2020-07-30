c27b880e8e9d5bed2dd941aa5159dac770ee9755
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-07-29 20:55:35 -0400

    Tests: Add product profile tests

---
+++ Northwind.Tests/UnitTests/Profiles/ProductProfileTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Northwind.API.Models;
+using Northwind.API.Profiles;
+using Northwind.Data.Entities;
+using Xunit;
+
+namespace Northwind.Tests.UnitTests.Profiles
+{
+    public class ProductProfileTests
+    {
+        private readonly IMapper _mapper;
+
+        public ProductProfileTests()
+        {
+            var mapperConfig = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new ProductProfile());
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public void ProductModelWithProductId_ProductModelToProduct_IgnoreProductId()
+        {
+            var productModel = new ProductModel
+            {
+                ProductId = 27,
+                ProductName = "Tofu"
+            };
+
+            var product = _mapper.Map<Product>(productModel);
+
+            Assert.Equal(0, product.ProductId);
+            Assert.Equal(0, product.SupplierId);
+            Assert.Equal("Tofu", product.ProductName);
+            Assert.Equal(0, product.UnitsInStock);
+        }
+
+        [Fact]
+        public void CategoryIdIsNull_ProductModelToProduct_IdSetToNull()
+        {
+            var productModel = new ProductModel
+            {
+                ProductId = 14,
+                ProductName = "Wine"
+            };
+
+            var product = _mapper.Map<Product>(productModel);
+
+            Assert.Null(product.CategoryId);
+        }
+    }
+}
