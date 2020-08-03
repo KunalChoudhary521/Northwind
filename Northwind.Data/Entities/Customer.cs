275464dac9455fee13ea7e8cfbe05c530378329b
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-08-03 13:43:13 -0400

    Data: Replace customer's address fields with Location

--- Northwind.Data/Entities/Customer.cs
+++ Northwind.Data/Entities/Customer.cs
@@ -11,18 +11,11 @@ namespace Northwind.Data.Entities
         public string CompanyName { get; set; }
         public string ContactName { get; set; }
         public string ContactTitle { get; set; }
+        public int LocationId { get; set; }
 
         // Navigation property
         public ICollection<Order> Orders { get; set; }
-
-        // TODO: Refactor: Move address fields in a new class
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Region { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
-        public string Phone { get; set; }
-        public string Fax { get; set; }
+        public Location Location { get; set; }
 
         public Customer()
         {
