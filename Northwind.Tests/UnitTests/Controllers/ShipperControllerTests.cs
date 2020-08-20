f3c48d4160994c0e3d85b14a3ef4aa33f4d8cfc2
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-08-19 20:53:47 -0400

    Tests: Add Shipper profile & controller unit tests

---
+++ Northwind.Tests/UnitTests/Controllers/ShipperControllerTests.cs
@@ -0,0 +1,122 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Northwind.API.Controllers;
+using Northwind.API.Models;
+using Northwind.API.Profiles;
+using Northwind.API.Services;
+using Northwind.Data.Entities;
+using Xunit;
+
+namespace Northwind.Tests.UnitTests.Controllers
+{
+    public class ShipperControllerTests
+    {
+        private readonly Mock<IShipperService> _shipperService;
+        private readonly ShippersController _shipperController;
+
+        public ShipperControllerTests()
+        {
+            var mapperConfig = new MapperConfiguration(config =>
+            {
+                config.AddProfiles(new Profile[] { new ShipperProfile() });
+            });
+
+            var mapper = mapperConfig.CreateMapper();
+            _shipperService = new Mock<IShipperService>();
+
+            _shipperController = new ShippersController(_shipperService.Object, mapper);
+        }
+
+        [Fact]
+        public async Task IdOfNonExistentShipper_GetShipper_ReturnNotFound()
+        {
+            const int shipperId = -1;
+
+            var response = await _shipperController.GetShipper(shipperId);
+
+            Assert.IsType<ActionResult<ShipperModel>>(response);
+            Assert.IsType<NotFoundResult>(response.Result);
+        }
+
+        [Fact]
+        public async Task FailedToSaveNewShipper_AddShipper_ReturnBadRequest()
+        {
+            var shipperModel = new ShipperModel();
+
+            var response = await _shipperController.AddShipper(shipperModel);
+
+            Assert.IsType<ActionResult<ShipperModel>>(response);
+            Assert.IsType<BadRequestResult>(response.Result);
+            _shipperService.Verify(sh => sh.Add(It.IsAny<Shipper>()));
+        }
+
+        [Fact]
+        public async Task ShipperModel_AddShipper_ReturnModelWithLocationInHeader()
+        {
+            var shipperModel = new ShipperModel();
+
+            _shipperService.Setup(sh => sh.IsSavedToDb()).ReturnsAsync(true);
+
+            var response = await _shipperController.AddShipper(shipperModel);
+
+            Assert.IsType<ActionResult<ShipperModel>>(response);
+            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(response.Result);
+
+            Assert.IsType<ShipperModel>(createdAtActionResult.Value);
+            Assert.Equal(StatusCodes.Status201Created, createdAtActionResult.StatusCode);
+            Assert.Single(createdAtActionResult.RouteValues.Keys, "shipperId");
+        }
+
+        [Fact]
+        public async Task IdOfNonExistentShipper_UpdateShipper_ShipperNotUpdated()
+        {
+            const int shipperId = -1;
+            var shipperModel = new ShipperModel();
+
+            var response = await _shipperController.UpdateShipper(shipperId, shipperModel);
+
+            Assert.IsType<ActionResult<ShipperModel>>(response);
+            Assert.IsType<NotFoundResult>(response.Result);
+        }
+
+        [Fact]
+        public async Task FailedToSaveUpdatedShipper_UpdateShipper_ReturnBadRequest()
+        {
+            const int shipperId = 7;
+
+            _shipperService.Setup(sh => sh.GetById(shipperId)).ReturnsAsync(new Shipper());
+
+            var response = await _shipperController.UpdateShipper(shipperId, new ShipperModel());
+
+            Assert.IsType<ActionResult<ShipperModel>>(response);
+            Assert.IsType<BadRequestResult>(response.Result);
+            _shipperService.Verify(sh => sh.Update(It.IsAny<Shipper>()));
+        }
+
+        [Fact]
+        public async Task IdOfNonExistentShipper_DeleteShipper_ShipperNotDeleted()
+        {
+            const int shipperId = -1;
+
+            var response = await _shipperController.DeleteShipper(shipperId);
+
+            Assert.IsType<NotFoundResult>(response);
+        }
+
+        [Fact]
+        public async Task FailedToSaveDeletedShipper_DeleteShipper_ReturnBadRequest()
+        {
+            const int shipperId = 3;
+
+            _shipperService.Setup(sh => sh.GetById(shipperId)).ReturnsAsync(new Shipper());
+
+            var response = await _shipperController.DeleteShipper(shipperId);
+
+            Assert.IsType<BadRequestResult>(response);
+            _shipperService.Verify(sh => sh.Delete(It.IsAny<Shipper>()));
+        }
+    }
+}
---
+++ Northwind.Tests/UnitTests/Profiles/ShipperProfileTests.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Northwind.API.Models;
+using Northwind.API.Profiles;
+using Northwind.Data.Entities;
+using Xunit;
+
+namespace Northwind.Tests.UnitTests.Profiles
+{
+    public class ShipperProfileTests
+    {
+        private readonly IMapper _mapper;
+
+        public ShipperProfileTests()
+        {
+            var mapperConfig = new MapperConfiguration(config =>
+            {
+                config.AddProfiles(new Profile[] { new ShipperProfile() });
+            });
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public void Shipper_ShipperToShipperModel_ReturnShipperModel()
+        {
+            var shipper = new Shipper
+            {
+                ShipperId = 12,
+                CompanyName = "Test shipper",
+                Phone = "(03) 3555-5011"
+            };
+
+            var shipperModel = _mapper.Map<ShipperModel>(shipper);
+
+            Assert.Equal(12, shipper.ShipperId);
+            Assert.Equal("Test shipper", shipper.CompanyName);
+            Assert.Equal("(03) 3555-5011", shipper.Phone);
+        }
+
+        [Fact]
+        public void ShipperModelWithShipperId_ShipperModelToShipper_ReturnShipperWithIdZero()
+        {
+            var shipperModel = new ShipperModel
+            {
+                ShipperId = 34,
+                CompanyName = "Test shipper model",
+                Phone = "416-901-5430"
+            };
+
+            var shipper = _mapper.Map<Shipper>(shipperModel);
+
+            Assert.Equal(0, shipper.ShipperId);
+            Assert.Equal("Test shipper model", shipper.CompanyName);
+            Assert.Equal("416-901-5430", shipper.Phone);
+        }
+    }
+}
