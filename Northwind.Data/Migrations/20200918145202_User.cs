05f5dbfdcc7d33f0b2567f108da9c13e412ccc9a
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-09-18 11:04:40 -0400

    Data: Add User migration

---
+++ Northwind.Data/Migrations/20200918145202_User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Northwind.Data.Migrations
+{
+    public partial class User : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserIdentifier = table.Column<Guid>(nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    UserName = table.Column<string>(nullable: true),
+                    PasswordSalt = table.Column<byte[]>(nullable: true),
+                    PasswordHash = table.Column<byte[]>(nullable: true),
+                    AccessToken = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: true),
+                    ExpiryDate = table.Column<DateTimeOffset>(nullable: true),
+                    CreateDate = table.Column<DateTimeOffset>(nullable: true),
+                    RevokeDate = table.Column<DateTimeOffset>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+        }
+    }
+}
